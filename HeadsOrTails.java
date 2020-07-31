9c2fe75f20989d0c0f81722ac5f32b28699ecf29
廣岡光輝 <hirookakouki@wlan-napt-001.u-aizu.ac.jp>
2020-07-31 11:00:00 +0900

    HoT: Initial revision

---
+++ HeadsOrTails.java
@@ -0,0 +1,31 @@
+
+public class HeadsOrTails {
+	final static int NumoRound = 3 , TossingTime = 5;
+	
+
+	public static void main(String[] args) {
+		int head_counter = 0,tail_counter = 0,head=0,tail=0;
+		
+		System.out.println("Tossing a coin...");
+		for(int i = 1 ; i <= NumoRound; i++) {
+			for(int j = 0 ; j < TossingTime;j++) {
+				if(0 == new java.util.Random().nextInt(2)) head_counter++;
+				else tail_counter++;
+			}
+			if(head_counter < tail_counter) {
+				System.out.println("Round "+i+": Tails");
+				tail++;
+			}
+			else if(head_counter > tail_counter){
+				System.out.println("Round "+i+": Heads");
+				head++;
+			}
+			else System.out.println("Draw...");
+			head_counter = 0;
+			tail_counter = 0;
+		}
+		System.out.println("Heads:"+head+", Tails:"+tail);
+		
+	}
+
+}
