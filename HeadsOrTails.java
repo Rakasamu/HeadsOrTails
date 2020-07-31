cf1135b5e3205b848c54b9f7b6b8ff8c96887664
廣岡光輝 <hirookakouki@wlan-napt-001.u-aizu.ac.jp>
2020-07-31 11:16:50 +0900

    User name added

--- HeadsOrTails.java
+++ HeadsOrTails.java
@@ -1,31 +1,36 @@
-
+import java.util.Scanner;
 public class HeadsOrTails {
-	final static int NumoRound = 3 , TossingTime = 5;
+    final static int NumoRound = 3 , TossingTime = 5;
 	
 
-	public static void main(String[] args) {
-		int head_counter = 0,tail_counter = 0,head=0,tail=0;
-		
-		System.out.println("Tossing a coin...");
-		for(int i = 1 ; i <= NumoRound; i++) {
-			for(int j = 0 ; j < TossingTime;j++) {
-				if(0 == new java.util.Random().nextInt(2)) head_counter++;
-				else tail_counter++;
-			}
-			if(head_counter < tail_counter) {
-				System.out.println("Round "+i+": Tails");
-				tail++;
-			}
-			else if(head_counter > tail_counter){
-				System.out.println("Round "+i+": Heads");
-				head++;
-			}
-			else System.out.println("Draw...");
-			head_counter = 0;
-			tail_counter = 0;
-		}
-		System.out.println("Heads:"+head+", Tails:"+tail);
-		
+    public static void main(String[] args) {
+	int head_counter = 0,tail_counter = 0,head=0,tail=0;
+	
+	System.out.println("Who are you?");
+	Scanner sc = new Scanner(System.in);
+	String name = sc.nextLine();
+	System.out.println("Hello,"+name+"!");
+	System.out.println("Tossing a coin...");
+	for(int i = 1 ; i <= NumoRound; i++) {
+	    for(int j = 0 ; j < TossingTime;j++) {
+		if(0 == new java.util.Random().nextInt(2)) head_counter++;
+		else tail_counter++;
+	    }
+	    if(head_counter < tail_counter) {
+		System.out.println("Round "+i+": Tails");
+		tail++;
+	    }
+	    else if(head_counter > tail_counter){
+		System.out.println("Round "+i+": Heads");
+		head++;
+	    }
+	    else System.out.println("Draw...");
+	    head_counter = 0;
+	    tail_counter = 0;
 	}
+	System.out.println("Heads:"+head+", Tails:"+tail);
+		
+    }
 
 }
+
